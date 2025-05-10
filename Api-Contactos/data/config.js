b8b398ede84ca650c75389112366baa4538e6ff7
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-10 12:34:30 -0600

    Carpeta data

---
+++ Api-Contactos/data/config.js
@@ -0,0 +1,17 @@
+// Dependencia de mongoose 
+const mongoose = require("mongoose");
+// Dependencia para el archivo .env
+require("dotenv").config();
+
+// Extraer en una url lo que contiene el .env
+const URL = process.env.URL;
+
+// Conexion a la base de datos 
+export const ConnectDB = async () => {
+try{
+    await mongoose.connect(URL);
+    console.log("Database running");
+}catch(error){
+    console.log("Cant connecto to database");
+}  
+}
