c61ec25132d2dfc1cc0f46060405ac34635f301d
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-10 13:21:41 -0600

    Modificaciones al modelo de base de datos

--- Api-Contactos/model/contact_model.js
+++ Api-Contactos/model/contact_model.js
@@ -2,6 +2,16 @@
 const mongoose = require("mongoose");
 // Definicion de la coleccion de la base de datos
 const contactSchema = new mongoose.Schema(
+    {
+        first_name: {
+            type: String, 
+            required:[true, "EL nombre es obligatorio"], 
+            trim: true,   
+            minlength: [2, "El nombre debe tener al menos dos caracteres"],
+            maxlength: [50,"El nombre no debe de rebasar los 50 caracteres"],
+        },
+        
 
-    
+    }
+
 );
