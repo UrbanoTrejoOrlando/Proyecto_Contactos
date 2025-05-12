f96fa155de0da62811c5909c6cdfd79d2986c7f9
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-11 18:37:25 -0600

    Modificacion a la sevices

--- Api-Contactos/services/contactServices.js
+++ Api-Contactos/services/contactServices.js
@@ -4,15 +4,16 @@ const Contactos = require("../model/contactModel");
 //Funcion para crear un contacto
 const CreateContact = async (data)=>{
     try{
-    // Creacion del libro
-    const newContact = new Contactos(data);
-    // Guardar el regsitro en la base de datos
-    await newContact.save()
-    // Retornar el nuevo contacto
-    return newContact;
+        // Creacion del libro
+        const newContact = new Contactos(data);
+        // Guardar el regsitro en la base de datos
+        await newContact.save()
+        // Retornar el nuevo contacto
+        return newContact;
 
     }catch(error){  
-    throw new Error("Error al crear el contacto" + error.message);
+        // Mensaje de errro si algo falla
+        throw new Error("Error al crear el contacto" + error.message);
 
     }
 };
