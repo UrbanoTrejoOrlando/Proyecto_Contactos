cac1c890ea0fbc73553ad1be6840d008d99e2473
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-11 18:25:59 -0600

    Carpeta routes

---
+++ Api-Contactos/routes/contactRoutes.js
@@ -0,0 +1,20 @@
+// Importamos express
+const express = require("express");
+// Creacion del enrutador modular
+const router = express.Router();
+const contactController = require("../controller/contactController");
+
+// Definicion de rutas
+// Ruta para crear un contacto
+router.post("/contacts", contactController.createContact);
+// Ruta para obtener todos los contactos
+router.get("/contacts", contactController.getAllContact);
+//Ruta para obtener un contacto
+router.get("/contacts/:contactid", contactController.getContactById);
+//Ruta para actualizar un contacto
+router.put("/contacts/:contactid", contactController.updateContact);
+// Ruta para eliminar un contacto
+router.delete("/contacts/:contactid", contactController.deleteContact);
+
+// Exportacion de las rutas
+module.exports = router;
