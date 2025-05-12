a4035ac4ac52695ef01fe02524f5dbd0e4328215
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-11 18:25:24 -0600

    Agregar a la carpeta controller

---
+++ Api-Contactos/controller/contactController.js
@@ -0,0 +1,83 @@
+// Obtener en una variable los servicios de la api
+const contactServices = require("../services/contactServices");
+
+// Peticion para crear un nuevo contacto
+const createContact = async (req, res) =>{
+    try {
+        // Guardar los datos del body en una variable
+        const contactData = req.body;
+        // Crear al contacto
+        const newContact = await contactServices.CreateContact(contactData);
+        // Configuracion del json
+        res.status(201).json({
+            message: "Contacto creado correctamente",
+            user: newContact,
+        });
+    } catch (error) {
+        res.status(400).json({
+            error: error.message,
+        });
+    }
+};
+
+// Peticion para obtener todos los contactos
+const getAllContact = async (req,res)=>{
+    try {
+         // Obtener todos los contactos
+        const contacts = await contactServices.GetAllContact();
+        res.status(200).json(contacts);
+
+    } catch (error) {
+        res.status(400).json({
+            error: ("Error fallo la conexion " + error.message),
+        });   
+    }    
+};
+
+// Peticion para obtener un contacto por el id
+const getContactById = async(req,res)=>{
+    const {contactid} = req.params;
+    try {
+        const contact = await contactServices.GetContactById(contactid);
+        res.status(200).json(contact);
+    } catch (error) {
+        res.status(400).json({
+            error: error.message,
+        });   
+    }
+};
+
+// Peticion para actualizar un contacto 
+const updateContact = async (req, res) =>{
+    const {contactid} = req.params;
+    const contactData = req.body;
+    try {
+        const updatecontact = await contactServices.UpdateContactById(contactid, contactData);
+        res.status(201).json({
+            message: "Contacto actualizado correctamente",
+            user: updatecontact,
+        })
+    } catch (error) {
+        res.status(400).json({
+            error: error.message,
+        }); 
+    }
+};
+
+
+// Peticion para eliminar un contacto
+const deleteContact = async (req,res) =>{
+    const {contactid} = req.params;
+    try {
+        const deletedcontact = await contactServices.DeleteContact(contactid);
+        res.status(201).json({
+            message: "Contacto eliminado",          
+        })
+    } catch (error) {
+        res.status(400).json({
+            error: error.message,
+        }); 
+    }
+};
+ 
+module.exports = {createContact,getAllContact,getContactById,updateContact,deleteContact};
