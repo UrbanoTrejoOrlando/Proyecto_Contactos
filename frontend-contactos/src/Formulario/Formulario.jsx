034e377bb9df9ad5ba8584cc2f4aed198d9d2f56
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-13 16:48:58 -0600

    Creacion de un nuevo componente

---
+++ frontend-contactos/src/Formulario/Formulario.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Formulario = () => {
+  return (
+    <div className=''>
+       <p class="text-gray-500 dark:text-gray-400">Hola Orlando</p>
+    </div>
+  )
+}
+
+export default Formulario
