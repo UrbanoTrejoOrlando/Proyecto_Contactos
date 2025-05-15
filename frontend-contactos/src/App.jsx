529771443d983ace4467e71a1436e4e3f1f1ef62
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-15 12:11:24 -0600

    Modificaciones al archivo principal

--- frontend-contactos/src/App.jsx
+++ frontend-contactos/src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './Layout/Layout'
+import Registrar from './Registrar/Registrar'
+import Visualizar from './Visualizar/Visualizar'
+
 
 const App = () => {
-  return (
-    <div>
-      <Layout/>
-    </div>
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+        <Route index element={<Registrar />} /> 
+        <Route path="registrar" element={<Registrar />} />
+        <Route path="visualizar" element={<Visualizar />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
+
 
 export default App
