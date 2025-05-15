eb62dd2a25f8bee3feacc5820deae8feeec6cbd8
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-15 13:15:00 -0600

    Modificaciones

--- frontend-contactos/src/Registrar/Registrar.jsx
+++ frontend-contactos/src/Registrar/Registrar.jsx
@@ -7,7 +7,7 @@ const Registrar = () => {
       <form className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
         <h2 className="text-2xl font-bold text-gray-800 text-center mb-6">Registrar Contacto</h2>
         
-        <div className="mb-4 flex items-center border rounded-xl px-4 py-2 focus-within:ring-2 focus-within:ring-green-400">
+        <div className="mb-4 flex items-center border rounded-xl px-4 py-2 focus-within:ring-2 focus-within:ring-blue-400">
           <CircleUserRound className="text-gray-400 w-5 h-5 mr-3" />
           <input 
             type="text"
@@ -17,7 +17,7 @@ const Registrar = () => {
           />
         </div>
 
-        <div className="mb-4 flex items-center border rounded-xl px-4 py-2 focus-within:ring-2 focus-within:ring-green-400">
+        <div className="mb-4 flex items-center border rounded-xl px-4 py-2 focus-within:ring-2 focus-within:ring-blue-400">
           <UserRound className="text-gray-400 w-5 h-5 mr-3" />
           <input 
             type="text"
@@ -27,7 +27,7 @@ const Registrar = () => {
           />
         </div>
         
-        <div className="mb-4 flex items-center border rounded-xl px-4 py-2 focus-within:ring-2 focus-within:ring-green-400">
+        <div className="mb-4 flex items-center border rounded-xl px-4 py-2 focus-within:ring-2 focus-within:ring-blue-400">
         <Mail className="text-gray-400 w-5 h-5 mr-3" />
           <input 
             type="email"
@@ -37,7 +37,7 @@ const Registrar = () => {
           />
         </div>
         
-        <div className="mb-4 flex items-center border rounded-xl px-4 py-2 focus-within:ring-2 focus-within:ring-green-400">
+        <div className="mb-4 flex items-center border rounded-xl px-4 py-2 focus-within:ring-2 focus-within:ring-blue-400">
         <Phone className="text-gray-400 w-5 h-5 mr-3" />
           <input 
             type="tel"
@@ -47,7 +47,7 @@ const Registrar = () => {
           />
         </div>
         
-        <div className="mb-4 flex items-center border rounded-xl px-4 py-2 focus-within:ring-2 focus-within:ring-green-400">
+        <div className="mb-4 flex items-center border rounded-xl px-4 py-2 focus-within:ring-2 focus-within:ring-blue-400">
         <MapPinHouseIcon className="text-gray-400 w-5 h-5 mr-3" />
           <input 
             type="text"
@@ -59,7 +59,7 @@ const Registrar = () => {
 
         <button 
           type="submit"
-          className="w-full bg-green-500 hover:bg-green-600 text-white font-semibold py-2 rounded-xl transition duration-200"
+          className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded-xl transition duration-200"
         >
           Registrar
         </button>
