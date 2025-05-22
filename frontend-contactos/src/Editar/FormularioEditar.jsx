12a7dbe9cbf47228a218174b94f207e018dc537e
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-22 13:14:49 -0600

    Modificaciones

--- frontend-contactos/src/Editar/FormularioEditar.jsx
+++ frontend-contactos/src/Editar/FormularioEditar.jsx
@@ -65,14 +65,16 @@ const FormularioEditar = () => {
     <div className="flex items-center justify-center">
       <form onSubmit={handleSubmit} className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
       <h2 className="text-2xl font-bold text-gray-800 text-center mb-6">Editar Contacto</h2>
-
-        <input
-          name="first_name"
-          value={contacto.first_name}
-          onChange={handleChange}
-          className="w-full p-2 border rounded"
-          placeholder="Nombre"
-        />
+        
+        <div className="mb-4 flex items-center border rounded-xl px-4 py-2 
+            focus-within:ring-2 focus-within:ring-blue-400">
+          <input
+            name="first_name"
+            value={contacto.first_name}
+            onChange={handleChange}
+            placeholder="Nombre"
+          />
+        </div>
         <input
           name="last_name"
           value={contacto.last_name}
