c5a4f281db8d313d77b0792cd5e61d41f89fe704
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-22 13:12:52 -0600

    Modificaciones

--- frontend-contactos/src/Editar/FormularioEditar.jsx
+++ frontend-contactos/src/Editar/FormularioEditar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { URL } from '../common/server';
+import { UserRound, CircleUserRound, Mail, Phone, MapPinHouseIcon } from 'lucide-react'
 
 const FormularioEditar = () => {
   // Obtener el ID de la URL
@@ -61,9 +62,10 @@ const FormularioEditar = () => {
   };
 
   return (
-    <div className="max-w-2xl mx-auto p-4 bg-white rounded shadow">
-      <h2 className="text-xl font-bold mb-4 text-center text-blue-700">Editar Contacto</h2>
-      <form onSubmit={handleSubmit} className="space-y-4">
+    <div className="flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
+      <h2 className="text-2xl font-bold text-gray-800 text-center mb-6">Editar Contacto</h2>
+
         <input
           name="first_name"
           value={contacto.first_name}
